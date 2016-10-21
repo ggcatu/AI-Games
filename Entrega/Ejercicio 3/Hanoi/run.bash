217479bab73ec6025eae796e7a2751af5e4b0852
Antoxx9 <anthonyj.elk@gmail.com>
2016-10-21 06:27:32 -0430

    Corridas ejercicio 3 listas

--- Entrega/Ejercicio 3/Hanoi/run.bash
+++ Entrega/Ejercicio 3/Hanoi/run.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> hanoi14_4.txt
+echo "grupo, algorithm, heuristic, domain, instance, cost, h0, generated, time, gen_per_sec" >> hanoi4_14.txt
 
 while read line
 do 
-  echo "$line" | timeout 100s ./4_14_Hanoi.dfs_2 >> hanoi14_4.txt &
+  echo "$line" | timeout 100s ./hanoi_4_14.dfs_1 >> hanoi4_14.txt &
 
   [ $(jobs | wc -l) -ge $(nproc) ] && wait
   

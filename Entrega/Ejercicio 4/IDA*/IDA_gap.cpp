a41ddd54a74a706c132e719f560c250ba03f172c
Eliot Diaz <eliot.daz@gmail.com>
2016-10-21 13:24:45 -0400

    Ahora si es casi definitivo.

--- Entrega/Ejercicio 4/IDA*/IDA_gap.cpp
+++ Entrega/Ejercicio 4/IDA*/IDA_gap.cpp
@@ -5,24 +5,25 @@
 #include <inttypes.h>
 #include <assert.h>
 #include <sys/time.h>
-#include <vector>
 #include <climits>
-#include <iostream>
+#include <time.h>
 using namespace std;
 
-state_t state, stateI;
+state_t initial;
+state_t state;
 state_t aux_state;
-vector<const char*> path;
-double t = -1;
+double tiempo = -1;
 double childCount = 0;
+int h0;
 
-void printing(int len){
-    printf("X, A*, gap, pancake28, \"");
-    print_state(stdout, &stateI);
-    if(len < 0){
-        printf("\", na, na, na, na\n");
+void printing(int len) {
+    printf("X, IDA*, gap, pancakes28, \"");
+    print_state(stdout, &initial);
+    if (len < 0) {
+        printf("\", %d, na, na, na, na\n", h0);
     } else {
-        printf("\", %d, %.0lf, %f, %.5e\n", len, childCount, t, childCount/t);
+        printf("\", %d, %d, %.0lf, %f, %.5e\n", h0, len, childCount, tiempo, 
+        	   childCount/tiempo);
     }
 }
 
@@ -30,6 +31,7 @@ void sig_handler(int SIG){
     printing(-1);
     exit(0);
 }
+
 unsigned int h_gap_28p(state_t state){
 	unsigned int gaps = 0;
 	for (int i = 0; i < 27; i++){
@@ -43,8 +45,8 @@ unsigned int h_gap_28p(state_t state){
 	return gaps;
 }
 
-pair<bool,unsigned int> f_bounded_dfs_visit(unsigned int bound, unsigned int g){
-	/* base-case */ 
+pair<bool,unsigned int> f_bounded_dfs_visit(unsigned int bound, unsigned int g, 
+											int history) {
 	pair<bool,unsigned> result;
 	unsigned int f = g + h_gap_28p(state);
 	if (f > bound) {
@@ -61,17 +63,23 @@ pair<bool,unsigned int> f_bounded_dfs_visit(unsigned int bound, unsigned int g){
 	unsigned int cost;
 	ruleid_iterator_t iter;
 	int ruleid;
+	int childHistory;
 	init_fwd_iter(&iter, &state);
-	while ((ruleid = next_ruleid(&iter)) >= 0) {
+	while ((ruleid = next_ruleid(&iter)) >= 0){
+		if (fwd_rule_valid_for_history(history, ruleid) == 0) {
+			printf("Hola boludo\n");
+			continue;
+		}
 		cost = g + get_fwd_rule_cost(ruleid);
+		printf("Ruleid: %d\n", ruleid);
+		childHistory = next_fwd_history(history, ruleid);
+		printf("Child history: %d\n", childHistory);
 		apply_fwd_rule(ruleid, &state, &aux_state);
 		copy_state(&state, &aux_state);
 		++childCount;
-		path.push_back(get_fwd_rule_label(ruleid));
-		result = f_bounded_dfs_visit(bound, cost);
+		result = f_bounded_dfs_visit(bound, cost, childHistory);
 		if (result.first) return result;
 		t = min(t, result.second);
-		path.pop_back();
 		apply_fwd_rule(ruleid, &state, &aux_state);
 		copy_state(&state, &aux_state);
 	}
@@ -82,9 +90,11 @@ pair<bool,unsigned int> f_bounded_dfs_visit(unsigned int bound, unsigned int g){
 
 unsigned int ida_search(){
 	unsigned int bound = h_gap_28p(state);
+	int history;
 	while (true) {
 		childCount = 0;
-		pair<bool,unsigned int> p = f_bounded_dfs_visit(bound, 0);
+		history = init_history;
+		pair<bool,unsigned int> p = f_bounded_dfs_visit(bound, 0, history);
 		if (p.first) return p.second;
 		bound = p.second;
 	}
@@ -98,19 +108,21 @@ int main(){
     ssize_t nchars; 
     // Leer un estado de la entrada estandar.
     //printf("Please enter a state followed by ENTER: ");
-    if( fgets(str, sizeof str, stdin) == NULL ) {
-        printf("Error: empty input line.\n");
+    if (fgets(str, sizeof str, stdin) == NULL) {
+        printf("Error: linea de entrada vacia.\n");
         return 0; 
     }
 
     // Convertir el string en un estado state_t.
     nchars = read_state(str, &state);
     if( nchars <= 0 ) {
-        printf("Error: invalid state entered.\n");
+        printf("Error: estado de entrada invalido.\n");
         return 0; 
     }
-    copy_state(&stateI,&state);
+
     // Algoritmo de busqueda IDA*
+    copy_state(&initial, &state);
+    h0 = h_gap_28p(initial);
     clock_t start = clock(), diff;
     try {
     	costo = ida_search();
@@ -120,18 +132,7 @@ int main(){
       exit(0);
     }
     diff = clock() - start; 
-    t = (double)diff / CLOCKS_PER_SEC;
+    tiempo = (double)diff / CLOCKS_PER_SEC;
     printing(costo);
-
-    // Impresion de resultados de la busqueda
-    //printf("The final state is: ");
-    //print_state(stdout, &state);
-    //printf("\n");
-    //printf("El numero de hijos explorados es: %d\n", childCount);
-    //printf("El costo de la solucion es: %d\n", costo);
-    //unsigned int size = path.size();
-    //cout << "Acciones realizadas: ";
-  	//for (unsigned i=0; i<size; i++) cout << ' ' << path[i];
-  	//cout << '\n';
     return 1;
 }
